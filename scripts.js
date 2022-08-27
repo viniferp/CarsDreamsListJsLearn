8fc85234fcf2f781fa10aa28a6e805c21b580417
vinicius_ferreirap <vinicius@vetorlog.com>
2022-08-27 01:12:50 -0300

    Beginning to create table dinamically.

--- scripts.js
+++ scripts.js
@@ -5,21 +5,55 @@ let cars = [];
 let brand = document.getElementById("fbrand-id");
 let name =  document.getElementById("lname-id");
 let year = document.getElementById("lyear-id");
-let AddBtn = document.getElementById("addCar-id");
-let ShowListBtn = document.getElementById("carsLists-id");
+let addBtn = document.getElementById("addCar-id");
+let showListBtn = document.getElementById("carsLists-id");
 const resetBtn = document.getElementById("reset-id");
 
+//Table Elements
+let table = document.createElement('table');
+let thead = document.createElement('thead');
+let tbody = document.createElement('tbody');
 
-AddBtn.onclick = function click() {
-    createCarsList(brand.value, name.value, year.value);
-    
-    
+
+
+
+let createTable = function() {
+    table.appendChild(thead);
+    table.appendChild(tbody)
+    document.getElementById('body').appendChild(table);
+
+    let row_1 = document.createElement('tr');
+    let heading_1 = document.createElement('th');
+    heading_1.innerHTML = "Brand";
+    let heading_2 = document.createElement('th');
+    heading_2.innerHTML = "Name";
+    let heading_3 = document.createElement('th');
+    heading_3.innerHTML = "Year";
+
+    row_1.appendChild(heading_1);
+    row_1.appendChild(heading_2);
+    row_1.appendChild(heading_3);
+    thead.appendChild(row_1);
+}
+
+
+createTable();
+
+addBtn.onclick = function click() {
+    if(brand != null && brand.value != '' && name != null && name.value != '', year != null && year.value !=''){
+        createCarsList(brand.value, name.value, year.value);
+        brand.value = "";
+        name.value = "";
+        year.value = "";
+    }
 };
+
+showListBtn.onclick = function() {
+    console.log(cars);
+}
 
 resetBtn.onclick = function click() {
     cars = [];
-    console.log(cars);
-    console.log('resetBen clicked!')
 };
 
 
