59f094a118c85e36cbc35e179e5483e51c407e79
vinicius_ferreirap <vinicius@vetorlog.com>
2022-08-30 15:06:09 -0300

    Table listing, but have some issues.

--- scripts.js
+++ scripts.js
@@ -11,54 +11,77 @@ const resetBtn = document.getElementById("reset-id");
 
 //Table Elements
 let table = document.createElement('table');
+table.setAttribute("id", "tableCars");
 let thead = document.createElement('thead');
 let tbody = document.createElement('tbody');
 
 
-
-
-let createTable = function() {
+let createUpdateTable = function() {
     table.appendChild(thead);
     table.appendChild(tbody)
-    document.getElementById('body').appendChild(table);
 
-    let row_1 = document.createElement('tr');
-    let heading_1 = document.createElement('th');
-    heading_1.innerHTML = "Brand";
-    let heading_2 = document.createElement('th');
-    heading_2.innerHTML = "Name";
-    let heading_3 = document.createElement('th');
-    heading_3.innerHTML = "Year";
+    let InitialTable =  document.getElementById('tableCars');
 
-    row_1.appendChild(heading_1);
-    row_1.appendChild(heading_2);
-    row_1.appendChild(heading_3);
-    thead.appendChild(row_1);
+    console.log(`initial: ${InitialTable}`);
+
+    if(InitialTable === null) {
+        document.getElementById('body').appendChild(table);
+        let row_1 = document.createElement('tr');
+
+        let heading_1 = document.createElement('th');   
+        let heading_2 = document.createElement('th');
+        let heading_3 = document.createElement('th');
+
+        heading_1.innerHTML = "Brand";
+        heading_2.innerHTML = "Name";
+        heading_3.innerHTML = "Year";
+
+        row_1.appendChild(heading_1);
+        row_1.appendChild(heading_2);
+        row_1.appendChild(heading_3);
+        thead.appendChild(row_1);
+    }
+
+    if(cars.length > 0) {
+        let row = document.createElement('tr');
+        
+        let column_1 = document.createElement('td');   
+        let column_2 = document.createElement('td');
+        let column_3 = document.createElement('td');
+
+        console.log("tste:: " + cars[cars.length - 1].brand);
+        column_1.innerHTML = cars[cars.length - 1].brand;
+        column_2.innerHTML = cars[cars.length - 1].name;
+        column_3.innerHTML = cars[cars.length - 1].year;
+
+        row.appendChild(column_1);
+        row.appendChild(column_2);
+        row.appendChild(column_3);
+        table.appendChild(row);
+    }
 }
 
-
-createTable();
-
 addBtn.onclick = function click() {
+    console.log(`brand: ${brand.value}`);
     if(brand != null && brand.value != '' && name != null && name.value != '', year != null && year.value !=''){
         createCarsList(brand.value, name.value, year.value);
         brand.value = "";
         name.value = "";
         year.value = "";
+    } else {
+        alert("Insert valid data!");
     }
 };
 
 showListBtn.onclick = function() {
-    console.log(cars);
+    createUpdateTable();
 }
 
 resetBtn.onclick = function click() {
     cars = [];
 };
 
-
-
-const createCarsList = function (brand = 'Renault', name = 'Megane Rs', year = '2020'){
+const createCarsList = function (brand, name, year){
     //ES5 - default values
     //brand = brand || 'Renault'
 
@@ -67,7 +90,5 @@ const createCarsList = function (brand = 'Renault', name = 'Megane Rs', year = '
         name, 
         year
     };
-
-    console.log(car);
     cars.push(car);
 };
